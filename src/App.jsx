e0f39ac379d15c2055c7d356e2558e51572847fc
Samantha <samantha@Samanthas-iMac.local>
2025-03-31 22:46:20 +0200

    📡 FAA App.jsx Final Route Injection

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Index from './pages/Index'
 import Meta from './pages/Meta'
+import Vault from './pages/Vault'
 
 function App() {
   return (
@@ -10,10 +11,10 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/index" element={<Index />} />
         <Route path="/meta" element={<Meta />} />
+        <Route path="/vault" element={<Vault />} />
       </Routes>
     </BrowserRouter>
   )
 }
 
 export default App
-
